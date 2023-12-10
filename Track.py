95201e23b233f70aa48afa4ace54160c915b23f4
Rapzzxploit <150666222+RapzzXploit@users.noreply.github.com>
2023-12-10 19:44:14 +0700

    Add files via upload

---
+++ Track.py
@@ -0,0 +1,57 @@
+import phonenumbers
+from phonenumbers import geocoder
+from key import keys
+
+
+
+print ("""
+ /$$$$$$$$                           /$$       /$$$$$$$  /$$                                    
+|__  $$__/                          | $$      | $$__  $$| $$                                    
+   | $$  /$$$$$$  /$$$$$$   /$$$$$$$| $$   /$$| $$  \ $$| $$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$ 
+   | $$ /$$__  $$|____  $$ /$$_____/| $$  /$$/| $$$$$$$/| $$__  $$ /$$__  $$| $$__  $$ /$$__  $$
+   | $$| $$  \__/ /$$$$$$$| $$      | $$$$$$/ | $$____/ | $$  \ $$| $$  \ $$| $$  \ $$| $$$$$$$$
+   | $$| $$      /$$__  $$| $$      | $$_  $$ | $$      | $$  | $$| $$  | $$| $$  | $$| $$_____/
+   | $$| $$     |  $$$$$$$|  $$$$$$$| $$ \  $$| $$      | $$  | $$|  $$$$$$/| $$  | $$|  $$$$$$$
+   |__/|__/      \_______/ \_______/|__/  \__/|__/      |__/  |__/ \______/ |__/  |__/ \_______/
+                                                                                                C0d3D By: DCM.X-505
+""")
+number = input("Enter phone number with country code:")
+
+
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number, "en")
+print(number, " : ", number_location)
+
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print("Service provider : ",carrier.name_for_number(service_provider, "en"))
+
+
+from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(keys)
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+timezone = results[0]['annotations']['timezone']['name']
+currency_name = results[0]['annotations']['currency']['name']
+currency_symbol = results[0]['annotations']['currency']['symbol']
+flag = results[0]['annotations']['flag']
+print("Time-Zone : ",timezone)
+print("Currency : ",currency_name)
+print("Symbol : ",currency_symbol)
+print("latitude: ",lat)
+print("longitude: ",lng)
+
+
+import folium
+map_location = folium.Map(location = [lat,lng], zoom_start=8)
+folium.Marker([lat,lng], popup=number_location).add_to(map_location)
+map_location.save("mylocation.html")
+
+import webbrowser
+# here insert the relative path of your mylocation.html file 
+url = " realtive path " 
+webbrowser.open_new(url)
